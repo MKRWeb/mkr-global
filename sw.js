87f6f38aa7eb232c9958baf5e0660d2bc2ebfd24
MKR Web <mkrweb2030@gmail.com>
2026-04-24 19:40:20 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mkr-global-cache-v18'; // Bumped cache version
+const CACHE_NAME = 'mkr-global-cache-v19'; // Bumped cache version
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
